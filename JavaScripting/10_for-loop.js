bea98d82b23928aa7392f47e1682a6c6873cfb5d
amikahmad <amikahmad@gmail.com>
2015-09-20 22:21:52 -0400

    i don't care

---
+++ JavaScripting/10_for-loop.js
@@ -0,0 +1,10 @@
+var total = 0;
+var limit = 10;
+
+for (var i = total; i < limit; i++) {
+    // on each iteration of the loop, add the number 'i' to the 'total' variable
+    total += i;
+}
+
+console.log(total);
+
