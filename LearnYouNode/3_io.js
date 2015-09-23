86388c997bb82f93e1a6a15696ba77f5d7a91c60
amikahmad <amikahmad@gmail.com>
2015-09-23 14:41:16 -0400

    yadayada

--- LearnYouNode/3_io.js
+++ LearnYouNode/3_io.js
@@ -1,16 +1,21 @@
 // Write a program that uses a single synchronous filesystem operation to read a file and print the number of newlines (\n) it contains to the console.
 
+// Load the filesystem (fs) module from the Node core library. This module loads all the required dependencies to perform filesystem functions.
 var fs = require('fs')
 
-// Return the file as a buffer object.
-var buffObj = fs.readFileSync(process.argv[2])
+// Read the file to output a buffer object and put into the variable 'contents'.
+var contents = fs.readFileSync(process.argv[2])
 
-// Convert the buffObj to string.
-var buffStr = buffObj.toString()
+// Buffer objects are used by Node to represent arbitrary arrays of data. We must convert this object to a string, then split the array and put each element into a new line.
+var lines = contents.toString().split('\n').length - 1
 
-// Count how many lines there are.
-// Use .split to create an array of substrings. Each split should be a new line.
-// In this example the test file  does not have a newline character so the array has one more element than desired. Simply subtract this.
-var lines = buffStr.split('\n').length - 1
-
+// Print the number of lines to the console
 console.log(lines)
+
+
+
+
+
+
+
+
---
+++ LearnYouNode/4_async.js
@@ -0,0 +1,13 @@
+// Write a program that uses a single asynchronous filesystem operation to read a file and print the number of newlines it contains to the console .
+
+var fs = require('fs')
+
+// fs.readFile('/etc/passwd', function (err, data) {
+// if (err) throw err;
+// console.log(data);
+// });
+
+fs.readFile(process.argv[2], function(err, contents) {
+    var lines = contents.toString().split('\n').length - 1;
+    console.log(lines);
+})
